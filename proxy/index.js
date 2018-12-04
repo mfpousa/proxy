e491901bef4d50bb788567f079b5a28797d86e79
Manuel Ferran <mfpousa@engineer.com>
2018-12-04 14:55:20 +0000

    Dummy

--- proxy/index.js
+++ proxy/index.js
@@ -3,5 +3,6 @@ const { parse } = require("url");
 module.exports = (req, res) => {
     const { query: { url } } = parse(req.url, true);
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Dummy", "Hi");
     request.get(url).pipe(res);
 };
